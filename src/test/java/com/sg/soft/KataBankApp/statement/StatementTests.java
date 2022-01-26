a2c123dc1ae1a27a1413754bf4c557383f8ff38c
walidHefhaf <whefhaf@alter-solutions.com>
2022-01-26 15:49:10 +0000

    added JUnit for unit testing, success and failure statements

---
+++ src/test/java/com/sg/soft/KataBankApp/statement/StatementTests.java
@@ -0,0 +1,40 @@
+package com.sg.soft.KataBankApp.statement;
+
+import com.sg.soft.KataBankApp.controller.StatementController;
+import com.sg.soft.KataBankApp.service.AccountService;
+import com.sg.soft.KataBankApp.service.StatementService;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class StatementTests {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private StatementService statementService;
+
+    @Mock
+    private AccountService accountService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        StatementController statementController = new StatementController(statementService, accountService);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(statementController).build();
+    }
+
+    @Test
+    void getStatementsTest() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/sg/soft/KataBankApp/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.sg.soft.KataBankApp.utils;
+
+public class Constants {
+
+
+    public static final Long ACCOUNT_NUMBER = 1234567890L;
+    public static final Long WRONG_ACCOUNT_NUMBER = 123L;
+
+    public static final String DEPOSIT = "DEPOSIT";
+    public static final String WITHDRAWAL = "WITHDRAWAL";
+
+    public static final Double STATEMENT_AMOUNT = 3000.00;
+}
