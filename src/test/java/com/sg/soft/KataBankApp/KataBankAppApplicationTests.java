1948ddffab68327c486cd12eb4872ccfd8dceb46
walidHefhaf <whefhaf@alter-solutions.com>
2022-01-26 18:04:54 +0000

    added test suite case for should make Deposit

--- src/test/java/com/sg/soft/KataBankApp/KataBankAppApplicationTests.java
+++ src/test/java/com/sg/soft/KataBankApp/KataBankAppApplicationTests.java
@@ -3,11 +3,13 @@ package com.sg.soft.KataBankApp;
 import com.sg.soft.KataBankApp.controller.StatementController;
 import com.sg.soft.KataBankApp.dto.AccountDTO;
 import com.sg.soft.KataBankApp.dto.StatementDTO;
+import com.sg.soft.KataBankApp.dto.StatementRequestDTO;
 import com.sg.soft.KataBankApp.service.AccountService;
 import com.sg.soft.KataBankApp.service.StatementService;
 import com.sg.soft.KataBankApp.utils.Constants;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -27,8 +29,11 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -50,6 +55,7 @@ class KataBankAppApplicationTests {
 	}
 
 	@Test
+	@DisplayName("Test suit for -> should get all statement success")
 	public void shouldGetStatementsTest() throws Exception {
 
 		AccountDTO accountDTO = new AccountDTO(Constants.ACCOUNT_NUMBER, Constants.BALANCE);
@@ -65,27 +71,53 @@ class KataBankAppApplicationTests {
 		assertEquals(expectedStatementsList.get(0).getStatementAmount(), Constants.STATEMENT_AMOUNT);
 
 		this.mockMvc.perform(get("/statement/")
-				.param("accountNumber", String.valueOf(Constants.ACCOUNT_NUMBER))
-				.contentType(MediaType.APPLICATION_JSON))
+					.param("accountNumber", String.valueOf(Constants.ACCOUNT_NUMBER))
+					.contentType(MediaType.APPLICATION_JSON))
+				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.content", hasSize(1)))
 				.andExpect(jsonPath("$.content[0].statementAmount", Matchers.is(Constants.STATEMENT_AMOUNT)));
 
 	}
 
-	@org.junit.Test
+	@Test
+	@DisplayName("Test suit for -> shoould not make deposit success")
 	public void should_Not_GetStatementsTest() throws Exception {
 
 	}
-	@org.junit.Test
+	@Test
+	@DisplayName("Test suit for -> should make deposit success")
 	public void shouldMakeDepositTest() throws Exception {
+		AccountDTO accountDTO = new AccountDTO(Constants.ACCOUNT_NUMBER, Constants.BALANCE);
+		StatementDTO expectedStatementDTO = new StatementDTO(1L, new Date(), Constants.STATEMENT_AMOUNT, Constants.DEPOSIT, accountDTO);
 
+		StatementRequestDTO statementRequestDTO = new StatementRequestDTO();
+		statementRequestDTO.setStatementAmount(Constants.STATEMENT_AMOUNT);
+		statementRequestDTO.setAccountNumber(Constants.ACCOUNT_NUMBER);
+
+		String preparedJsonRequest = "{\"accountNumber\":\""
+				+ statementRequestDTO.getAccountNumber() + "\",\"statementAmount\":\""
+				+ statementRequestDTO.getStatementAmount() +"\"}";
+
+		when(statementService.makeDeposit(Constants.ACCOUNT_NUMBER, Constants.STATEMENT_AMOUNT)).thenReturn(expectedStatementDTO);
+
+		assertEquals(expectedStatementDTO.getStatementAmount(), Constants.STATEMENT_AMOUNT);
+		assertEquals(expectedStatementDTO.getAccount().getAccountNumber(), Constants.ACCOUNT_NUMBER);
+
+		this.mockMvc.perform(post("/statement/deposit")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(preparedJsonRequest))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
-	@org.junit.Test
+
+	@Test
+	@DisplayName("Test suit for -> should make withdrawal success")
 	public void shouldMakeWithdrawalTest() throws Exception {
 
 	}
-	@org.junit.Test
+	@Test
+	@DisplayName("Test suit for -> should not make withdrawal success")
 	public void should_Not_MakeWithdrawalTest() throws Exception {
 
 	}
