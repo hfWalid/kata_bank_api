a08ab71e31733f91c987107603093e899f33f91b
walidHefhaf <whefhaf@alter-solutions.com>
2022-01-26 13:37:46 +0000

    kata project init

---
+++ src/main/java/com/sg/soft/KataBankApp/KataBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.sg.soft.KataBankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataBankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/soft/KataBankApp/KataBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.soft.KataBankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
